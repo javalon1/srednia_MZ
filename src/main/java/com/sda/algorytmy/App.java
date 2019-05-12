25f6a469b597dc5651d2f5b5335449e1649357ff
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-12 15:52:59 +0100

    wersja ostateczna

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,32 @@
 package com.sda.algorytmy;
+import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
-public class App 
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int wynik = 0;
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            wynik = wynik + a;
+
+        }
+        wynik = wynik / n;
+        System.out.println(wynik);
+
     }
 }
+
+
+
